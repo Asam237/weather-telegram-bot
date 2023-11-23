6c0524b7d8c0d277c254ccd5229f3e3b8b2aff2d
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-23 09:23:30 +0100

    remove unused imports

--- bot_modules/help/index.js
+++
@@ -1,5 +0,0 @@
-const help = `
-/hi - Say hello to bot
-/weather - Weather in Yaounde`;
-
-export { help };
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import TelegramBot from "node-telegram-bot-api";
 import { TELEGRAM_BOT } from "./shared/core/config.js";
-import { city, get } from "./bot_modules/weather/index.js";
-import { help } from "./bot_modules/help/index.js";
 import axios from "axios";
 
 const bot = new TelegramBot(TELEGRAM_BOT, { polling: true });
+
+const getWeather = async () => {
+  const AxiosInstance = axios.create();
+  await AxiosInstance.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_BOT}&units=metric`
+  )
+    .then((response) => {
+      console.log("Response ->", response.data);
+      return response.data;
+    })
+    .catch(console.error);
+};
+
+const help = `
+/hi - Say hello to bot
+/weather - Weather in Yaounde`;
 
 bot.onText(/\/hi/, (msg) => {
   const chatId = msg.chat.id;
@@ -19,16 +33,5 @@ bot.onText(/\/help/, (msg) => {
 bot.onText(/\/weather/, (msg) => {
   const chatId = msg.chat.id;
   city = msg.text.split(" ")[1] ? msg.text.split(" ")[1] : "Yaounde";
-  bot.sendMessage(
-    chatId,
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_BOT}&units=metric`
-      )
-      .then((response) => {
-        console.log("Response ->", response.data);
-        return response.data;
-      })
-      .then((weather) => bot.sendMessage(chatId, weather))
-  );
+  bot.sendMessage(chatId, bot.sendMessage(chatId, getWeather));
 });
