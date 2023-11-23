adf347d0ccb77250c03b8c55481b48fad1955eaf
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-23 16:58:53 +0100

    refactoring

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ import axios from "axios";
 const bot = new TelegramBot(TELEGRAM_BOT, { polling: true });
 
 const help = `
-/start -🐰 Start 
-/hi - 👋  Say hello to bot
+/start - 👋  Start to bot
+/help -🐰 Help section
 /weather - 🌦️ Weather in Yaounde`;
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, help);
+  bot.sendMessage(chatId, `Welcome to your Telegram bot ${msg.from.first_name} 😊\nType /help to explore the various options.`);
 });
 
-bot.onText(/\/hi/, (msg) => {
+bot.onText(/\/help/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    `Welcome to your Telegram bot 😊, ${msg.from.first_name}`
-  );
+  bot.sendMessage(chatId, help);
 });
 
 bot.onText(/\/weather/, async (msg) => {
