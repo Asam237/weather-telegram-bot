bee4a9182d9340b6a6b83eded75c5d13a6e70e64
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-24 09:10:17 +0100

    lyric search lib

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 import { TELEGRAM_BOT, WEATHER_BOT } from "./shared/core/config.js";
-import lyricsFinder from "lyrics-finder";
+import lyricsSearcher from "lyrics-searcher";
 import axios from "axios";
 
 const bot = new TelegramBot(TELEGRAM_BOT, { polling: true });
@@ -51,9 +51,10 @@ bot.onText(/\/weather/, async (msg) => {
 bot.onText(/\/lyrics (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
   const songName = match[1];
+  const songTitle = match[2];
 
   try {
-    const lyrics = await getLyrics(songName);
+    const lyrics = await getLyrics(songName, songTitle);
     if (lyrics) {
       bot.sendMessage(chatId, `Lyrics for "${songName}":\n${lyrics}`);
     } else {
@@ -65,7 +66,14 @@ bot.onText(/\/lyrics (.+)/, async (msg, match) => {
   }
 });
 
-const getLyrics = async (songName) => {
-  const lyrics = await lyricsFinder(songName);
+const getLyrics = async (songName, songTitle) => {
+  const lyrics = await lyricsSearcher(songName, songTitle)
+    .then((lyrics) => {
+      console.log(lyrics);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   return lyrics;
 };
