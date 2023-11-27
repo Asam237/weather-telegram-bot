19d7a47f8bdc62ee8a62aa1b37c2abc612e1d9a2
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-27 16:20:01 +0100

    remove bot lyric

--- index.js
+++ index.js
@@ -47,23 +47,6 @@ bot.onText(/\/weather/, async (msg) => {
   }
 });
 
-bot.onText(/\/lyrics (.+)/, async (msg, match) => {
-  const chatId = msg.chat.id;
-  const songName = match[1];
-
-  try {
-    const lyrics = await getLyrics(songName);
-    if (lyrics) {
-      bot.sendMessage(chatId, `Lyrics for "${songName}":\n${lyrics}`);
-    } else {
-      bot.sendMessage(chatId, `Lyrics for "${songName}" not found.`);
-    }
-  } catch (error) {
-    console.error("Error fetching lyrics:", error);
-    bot.sendMessage(chatId, "Error fetching lyrics. Please try again later.");
-  }
-});
-
 const getLyrics = async (songName) => {
   try {
     const lyrics = await lyricsFinder(songName);
