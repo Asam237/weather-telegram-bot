cb7eb9c0957fb950914e4d415f981f01ba527535
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-29 16:09:41 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 import TelegramBot from "node-telegram-bot-api";
-import { TELEGRAM_BOT, WEATHER_BOT } from "./shared/core/config.js";
 import axios from "axios";
+import { TELEGRAM_BOT, WEATHER_BOT } from "./shared/core/config.js";
 
 const bot = new TelegramBot(TELEGRAM_BOT, { polling: true });
 
-const help = `
+const helpMessage = `
 /start - 👋 Start to bot
-/help -🐰 Help section
+/help - 🐰 Help section
 /weather - 🌦️ Weather in Yaounde`;
 
-bot.onText(/\/start/, (msg) => {
+const sendWelcomeMessage = (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(
     chatId,
     `Welcome to your Telegram bot ${msg.from.first_name} 😊\nType /help to explore the various options.`
   );
-});
+};
 
-bot.onText(/\/help/, (msg) => {
+const sendHelpMessage = (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, help);
-});
+  bot.sendMessage(chatId, helpMessage);
+};
 
-bot.onText(/\/weather/, async (msg) => {
+const sendWeatherInfo = async (msg) => {
   const chatId = msg.chat.id;
-  const city = msg.text.split(" ")[1] ? msg.text.split(" ")[1] : "yaounde";
-  const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_BOT}&units=metric`
-  );
+  const city = msg.text.split(" ")[1] || "yaounde";
+
   try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_BOT}&units=metric`
+    );
+
     const data =
       "Current weather in: " +
       response.data.name +
@@ -36,6 +38,7 @@ bot.onText(/\/weather/, async (msg) => {
       response.data.main.temp +
       "°C\nDescription: " +
       response.data.weather[0].description;
+
     bot.sendMessage(chatId, data);
   } catch (error) {
     console.error(`Error fetching: ${error.message}`);
@@ -44,5 +47,10 @@ bot.onText(/\/weather/, async (msg) => {
       "Sorry, an error occurred while fetching weather."
     );
   }
-});
+};
 
+bot.onText(/\/start/, sendWelcomeMessage);
+
+bot.onText(/\/help/, sendHelpMessage);
+
+bot.onText(/\/weather/, sendWeatherInfo);
